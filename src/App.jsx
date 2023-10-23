3934ca40890d5a9894909147e0f1b2221b7eb7dc
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-10-23 17:31:20 +0530

    23-10-2023

--- src/App.jsx
+++ src/App.jsx
@@ -12,5 +12,36 @@ import useDisclouse from "./hooks/useDisclouse";
 import NotFoundContact from "./components/NotFoundContact";
 
 
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+
+  const { isOpen, onClose, onOpen } = useDisclouse();
+
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        const contactReff = collection(db, "contacts");
+
+        onSnapshot(contactReff, (snapshot) => {
+          const contactLists = snapshot.docs.map((doc) => {
+            return {
+              id: doc.id,
+              ...doc.data(),
+            };
+          });
+
+          setContacts(contactLists);
+          return contactLists;
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getContacts();
+  }, []);
+
+  
+};
 
 export default App;
