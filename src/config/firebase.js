8334f83f5f7084638895341e382278e4401877bd
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-10-24 20:30:19 +0530

    :boom: :boom:  Firebase  Setup  :boom: :boom:

---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC7zVWCdzvAFNCek_6roTvFdXRhQuRrITI",
+  authDomain: "vite-contact-apk.firebaseapp.com",
+  projectId: "vite-contact-apk",
+  storageBucket: "vite-contact-apk.appspot.com",
+  messagingSenderId: "679769905508",
+  appId: "1:679769905508:web:03bc92a65e3a06129ec271"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
