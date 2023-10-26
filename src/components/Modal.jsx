54b32d92a99f13d2a0868df7c3eca84ab1852590
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-10-26 22:37:34 +0530

    26-10-2023

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,7 +4,11 @@ import { AiOutlineClose } from "react-icons/ai";
 const Modal = ({ onClose, isOpen, children }) => {
   return createPortal(
     <>
-      
+      {isOpen && (
+        <div className="absolute top-0 z-40 grid h-screen w-screen place-items-center backdrop-blur">
+          
+        </div>
+      )}
     </>,
     document.getElementById("modal-root")
   );
