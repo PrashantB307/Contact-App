0873001ab4234064925ff32f4ac3b800b2ff3c87
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-10-28 22:53:22 +0530

    :boom: :boom:  Contact Not Found  :boom: :boom:

--- src/components/NotFoundContact.jsx
+++ src/components/NotFoundContact.jsx
@@ -1,7 +1,12 @@
 
 const NotFoundContact = () => {
   return (
-    
+    <div className="flex pt-20 items-center justify-center">
+        {/* <img className="text-2xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDl-sr2Tn4Fc6tl8U2FgbehD5HY3QIGQv3vw"/> */}
+        <h3 className=" text-2xl font-bold text-white ">
+            Conatct Not Found
+        </h3>
+    </div>
   )
 }
 
